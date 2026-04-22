0d5e689d4c870fa9e35d450bbc8acbc87006f62a
GreenCoderPrashu <pr829639@gmail.com>
2026-04-22 21:06:08 +0530

    Created Hambergar manu.

---
+++ assets/documents/script.js
@@ -0,0 +1,6 @@
+function togglemenu(){
+    const menu = document.querySelector(".Menu-Links");
+    const icon = document.querySelector(".Ham-Icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
